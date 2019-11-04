86af727ac71175e8d06cfde8a9ffe11fc22a2bd6
Rodrigo Rossi <rdrg.rossi@gmail.com>
2019-11-04 12:03:22 -0200

    Segundo commit

--- src/main/java/br/com/prisma/requisicaovaga/model/Employee.java
+++ src/main/java/br/com/prisma/requisicaovaga/model/Employee.java
@@ -1,7 +1,9 @@
 package br.com.prisma.requisicaovaga.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.Objects;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Employee {
 
     private String employeeId;
