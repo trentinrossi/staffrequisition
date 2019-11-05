7b6bf4fcf61253ac8d361b25568f1caf5d43c608
Rodrigo Rossi <rdrg.rossi@gmail.com>
2019-11-05 13:00:59 -0200

    Segundo commit

--- src/main/java/br/com/prisma/requisicaovaga/resource/VacancyInterceptor.java
+++ src/main/java/br/com/prisma/requisicaovaga/resource/VacancyInterceptor.java
@@ -1,9 +1,12 @@
 package br.com.prisma.requisicaovaga.resource;
 
 import br.com.prisma.requisicaovaga.model.StaffRequisition;
+import br.com.prisma.requisicaovaga.model.StaffRequisitionReason;
 import br.com.prisma.requisicaovaga.service.StaffRequisitionService;
+import java.util.HashMap;
 import java.util.Map;
 import javax.inject.Inject;
+import javax.ws.rs.core.Request;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.ResponseEntity;
@@ -24,24 +27,30 @@ public class VacancyInterceptor {
     }
 
     @PostMapping(path = "/createStaffRequisition")
-    public ResponseEntity<?> post(@RequestBody StaffRequisition request,
+    public ResponseEntity<?> post(@RequestBody HashMap<String, String> request,
             @RequestHeader Map<String, String> headers,
             @RequestHeader(value = "x-senior-token", required = false) String token) {
 
         System.out.println("Nova requisição: " + request);
 
+        StaffRequisition req = new StaffRequisition();
+        req.setReason(StaffRequisitionReason.valueOf(request.get("reason")));
+        req.setReplacedEmployeeId(request.get("replacedEmployeeId"));
+        req.setActiveEmployeeId(request.get("activeEmployeeId"));
+        
+        System.out.println(req);
+        
 //        headers.forEach((key, value) -> {
 //            System.out.println(String.format("Header '%s' = %s", key, value));
 //        });
         try {
-            if (service.isPresentInOthersRequisitions(request, token)) {
+            if (service.isPresentInOthersRequisitions(req, token)) {
                 return ResponseEntity.badRequest().body("Colaborador já informado em outras requisições/vagas");
             } else {
                 return ResponseEntity.ok().body(request);
             }
         } catch (Exception ex) {
             return ResponseEntity.badRequest().body(ex.getMessage());
-        }
-
+        }        
     }
 }
